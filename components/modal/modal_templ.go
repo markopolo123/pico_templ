78d95c4d53feb208c1cee2899d6be03f89c9af89
coral <698889+markopolo123@users.noreply.github.com>
2026-01-17 20:47:28 +0000

    fix: align templ version and exclude _templ.go from goimports
    - Downgrade go.mod templ to v0.3.960 to match flox
    - Exclude _templ.go files from goimports (templ generates its own format)

--- components/modal/modal_templ.go
+++ components/modal/modal_templ.go
@@ -7,10 +7,8 @@ package modal
 
 //lint:file-ignore SA4006 This context is only used if a nested component is present.
 
-import (
-	"github.com/a-h/templ"
-	templruntime "github.com/a-h/templ/runtime"
-)
+import "github.com/a-h/templ"
+import templruntime "github.com/a-h/templ/runtime"
 
 // Props configures the main Modal dialog component.
 type Props struct {
